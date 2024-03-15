7a8f52febe2b2c56a03af4b5603918956fcec325
Maggy Tjin <tjin.maggy@gmail.com>
2024-03-15 14:29:22 -0700

    first tests 4 passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,20 @@
 
 const removeFromArray = function(array, a, b) {
-    let removeDup = array.filter((value, index) => array.indexOf(value) === index);
-    let indexA = removeDup.indexOf(a);
-    const removeA = removeDup.splice(indexA, 1);
+    if (!array.includes(b) && !array.includes(a)){
+      return array;
+    } 
+    let removeDupe = array.filter((value, index) => array.indexOf(value) === index);
     
+    let indexA = removeDupe.indexOf(a);
+    const removeA = removeDupe.splice(indexA, 1);
+      
     if (b) {  
-      let indexB = removeDup.indexOf(b);
-      const removeB = removeDup.splice(indexB, 1);
+      let indexB = removeDupe.indexOf(b);
+      const removeB = removeDupe.splice(indexB, 1);
     };
+    
+    return removeDupe;
   
-    return removeDup;
   };
 
 
