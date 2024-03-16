6b5bcc44d6a87bae3d43536e64ebbed750cc03bd
Maggy Tjin <tjin.maggy@gmail.com>
2024-03-15 23:19:34 -0700

    2nd test passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
-
-};
+const sumAll = function(a, b) {
+    let sum = 0;
+    let array = Array.from(arguments);
+    let fullArr = [];
+    
+    for (let i = array[0]; i <= array[1]; i++){
+        sum += i;
+    }
+    return sum;
+  };
 
 // Do not edit below this line
 module.exports = sumAll;
