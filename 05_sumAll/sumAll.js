37b359a0b97c66afdc79c9c7bc749abaea445270
Maggy Tjin <tjin.maggy@gmail.com>
2024-03-18 19:58:03 -0700

    completed tempConversion

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,13 +1,23 @@
-const sumAll = function(a, b) {
+const sumAll = function(min, max) {
+	
+    if (!Number.isInteger(min) || !Number.isInteger(max)) {
+        return "ERROR";
+    }
+    if (min < 0 || max < 0) {
+        return "ERROR"
+    }
+    if (min > max) {
+        const temp = min; 
+        min = max; 
+        max = temp; 
+    }
+
     let sum = 0;
-    let array = Array.from(arguments);
-    let fullArr = [];
-    
-    for (let i = array[0]; i <= array[1]; i++){
+    for (let i = min; i <= max; i++){
         sum += i;
     }
     return sum;
-  };
+}
 
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+	const final = (f - 32) * (5 / 9);
+	return parseFloat(final.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+	const final = (c * (9 / 5)) + 32;
+	return parseFloat(final.toFixed(1));
 };
 
 // Do not edit below this line
