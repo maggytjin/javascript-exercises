2f0da52043ec78f65979b44a78986137df722607
Maggy Tjin <tjin.maggy@gmail.com>
2024-03-31 08:55:43 -0700

    completed calculator and palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+	let newWord = word
+		.toLowerCase()
+		.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '')
+		.split(" ")
+		.join("");
+	// return newWord;
+	let reversed = newWord
+                    .split("")
+                    .reverse()
+                    .join("");
+	if (newWord === reversed) {
+		return true;
+	}
+	return false;
 };
 
 // Do not edit below this line
