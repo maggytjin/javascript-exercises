f42eb710d7dccbeca727604bac0bd1f72b8c1a59
Maggy Tjin <tjin.maggy@gmail.com>
2024-03-31 20:51:57 -0700

    completed 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+		return "OOPS";
+	} else if (num == 0) {
+        return 0;
+    } else if (num <= 1) {
+        return 1;
+    }
+	let num1 = 0;
+	let num2 = 1;
+	let sum;
+	let i;
+	for (i = 2; i <= num; i++) {
+		sum = num1 + num2;
+		num1 = num2;
+		num2 = sum;
+	}
+	return num2;
 };
 
 // Do not edit below this line
