ddb1742d6064c9720d86d99100759cc42b9a7c84
Maggy Tjin <tjin.maggy@gmail.com>
2024-04-03 14:10:51 -0700

    completed to return object

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    let currentYear;
+    for (let i = 0; i < array.length; i++) {
+        if (!array[i].yearOfDeath) {
+            let currentDate = new Date();
+            currentYear = currentDate.getFullYear();
+            array[i].yearOfDeath = currentYear;
+        }
+    };
+    
+	for (let i = 0; i < array.length; i++) {
+		let age = array[i].yearOfDeath - array[i].yearOfBirth;
+		array[i].age = age;
+	}
+	let sorted = array.sort((a, b) => b.age - a.age);
+	return sorted[0];
 };
 
 // Do not edit below this line
